1ffc4032b28ed08271c44ecd5cc4d2e92c048396
Grady253 <grady.andre2@gmail.com>
2022-06-17 12:38:34 -0700

    I started to work on my if else statement.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordspecialCharacters;//This is for the special characters. 
+var passwordLength;// this is for the length of the password.
+var passwordCase;// Weather its upper case or lower case.
+var passwordNum; // Numbers for the generate password.
+
 
 
 function generatePassword() {
+  console.log("you did it");
+  passwordLength= prompt("Please enter the password length 8-128");
+
+  if(passwordLength >= 8 && passwordLength <= 128){
+    (confirm("Do you wish to have uppercase,lowercase, numbers, and symbols?"));
+  } else{
+    alert("Sorry that amount doesn't meet our password requirements.");
+  }
 
 }
 // Write password to the #password input
