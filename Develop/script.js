d5b8cff616b94b8f73d4c29c91cad5cbce711eed
Grady253 <grady.andre2@gmail.com>
2022-06-17 09:06:45 -0700

    I've added variables for upper case, lower case, numbers, and special characters.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var upperCaseLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+  var lowerCaseLetter =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","m","w","x","y","z"];
+
+  var passwordNumber  = [1,2,3,4,5,6,7,8,9,0];
+
+  var passwordCharacter =["!","#",'"',"$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","_","{","|","}","`","~"];
+
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
