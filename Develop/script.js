aa56887e92349df535b07e19a5d6fba910457ec9
Grady253 <grady.andre2@gmail.com>
2022-06-22 10:09:00 -0700

    I changed my if statements inside my function generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -6,13 +6,38 @@ var passwordCase;// Weather its upper case or lower case.
 var passwordNum; // Numbers for the generate password.
 
 
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+var lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","m","w","x","y","z"];
+
+var passwordNumber  = [1,2,3,4,5,6,7,8,9,0];
+
+var specialCharacter =  ["!","#",'"',"$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","_","{","|","}","`","~"];
 
 function generatePassword() {
   console.log("you did it");
   passwordLength= prompt("Please enter the password length 8-128");
 
   if(passwordLength >= 8 && passwordLength <= 128){
-    (confirm("Do you wish to have uppercase,lowercase, numbers, and symbols?"));
+    confirmPrompt();
+    result="";
+    length=0;
+      if(upperCase == true && length<passwordLength){
+        result = result + upperCase[Math.floor(Math.random() *26)]
+        length++
+      };
+      if (lowerCase == true && length < passwordLength){
+        result = result + lowerCase[Math.floor(Math.random() *26)]
+        length++
+      };
+      if (passwordNumber == true && length < passwordLength){
+        result = result + passwordNumber[Math.floor(Math.random()*10)]
+        length++
+      };
+      if (specialCharacter == true && length < passwordLength){
+        result = result + specialCharacter[Math.floor(Math.random()*29)]
+        length++
+      };
   } else{
     alert("Sorry that amount doesn't meet our password requirements.");
   }
@@ -24,16 +49,9 @@ function writePassword() {
 
   var passwordText = document.querySelector("#password");
 
-  var upperCaseLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-  var lowerCaseLetter =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","m","w","x","y","z"];
-
-  var passwordNumber  = [1,2,3,4,5,6,7,8,9,0];
-
-  var passwordCharacter =["!","#",'"',"$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","_","{","|","}","`","~"];
-
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click",password);
