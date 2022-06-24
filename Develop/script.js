60d0c38bcf5ffc3a8a9ab02d79d9599af7702080
Grady253 <grady.andre2@gmail.com>
2022-06-24 09:28:49 -0700

    Added my function for my confirm messages to pop-up

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,73 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordspecialCharacters;//This is for the special characters. 
+var specialCharacters;//This is for the special characters. 
 var passwordLength;// this is for the length of the password.
-var passwordCase;// Weather its upper case or lower case.
-var passwordNum; // Numbers for the generate password.
+var upperCase;// Weather its upper case.
+var lowerCase;// Weather it's lower case.
+var passwordNumber; // Numbers for the generate password.
 
-
+// My varibles that contain strings of letters, numbers, and characters to generate a random password.
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","m","w","x","y","z"];
+var lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 var passwordNumber  = [1,2,3,4,5,6,7,8,9,0];
 
 var specialCharacter =  ["!","#",'"',"$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","_","{","|","}","`","~"];
 
+
+// this is my function to get my random password generated. 
 function generatePassword() {
-  console.log("you did it");
   passwordLength= prompt("Please enter the password length 8-128");
 
   if(passwordLength >= 8 && passwordLength <= 128){
     confirmPrompt();
     result="";
     length=0;
-      if(upperCase == true && length<passwordLength){
+      if(upperCase === true && length<passwordLength){
         result = result + upperCase[Math.floor(Math.random() *26)]
         length++
       };
-      if (lowerCase == true && length < passwordLength){
+      if (lowerCase === true && length < passwordLength){
         result = result + lowerCase[Math.floor(Math.random() *26)]
         length++
       };
-      if (passwordNumber == true && length < passwordLength){
+      if (passwordNumber === true && length < passwordLength){
         result = result + passwordNumber[Math.floor(Math.random()*10)]
         length++
       };
-      if (specialCharacter == true && length < passwordLength){
+      if (specialCharacter === true && length < passwordLength){
         result = result + specialCharacter[Math.floor(Math.random()*29)]
         length++
       };
   } else{
     alert("Sorry that amount doesn't meet our password requirements.");
+    generatePassword();
   }
+  return result;
+};
 
-}
+
+function confirmPrompt(){
+  upperCase = confirm("Would you like to have Upper case letters in your password?");
+
+  lowerCase = confirm("Would you like to have lower case letters in your password?");
+
+  passwordNumber = confirm("Would you like to have numbers in your password?");
+
+  specialCharacter = confirm("Would you like to have special characters in your password?");
+};
+
+
 // Write password to the #password input
-function writePassword() {
+function writePassword(event) {
   var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  event.preventDefault();
 }
 
 // Add event listener to generate button
